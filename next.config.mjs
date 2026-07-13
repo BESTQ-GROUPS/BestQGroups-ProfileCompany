459a852165ce32845eb936250f5f4d57f5840473
PrastioRifkiWijaya <wijayaprastio23@gmail.com>
2026-07-14 02:45:25 +0700

    fix: change next.config.ts to .mjs for next 14 compat

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 if (process.env.NODE_ENV === 'development') {
   setupDevPlatform({ persist: true });
 }
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
